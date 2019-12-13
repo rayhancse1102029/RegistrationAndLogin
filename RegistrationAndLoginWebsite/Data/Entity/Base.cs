fe62f9620e38e14d397f4703fe5fc54a6db18378
aburayhan <rayhanabu447@gmail.com>
2019-12-13 12:28:04 +0600

    Base Class Add

---
+++ RegistrationAndLoginWebsite/Data/Entity/Base.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistrationAndLoginWebsite.Data
+{
+    public class Base
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public string UpdatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public int IsDeleted { get; set; }
+
+        public int IsBlocked { get; set; }
+
+    }
+}
--- RegistrationAndLoginWebsite/Startup.cs
+++ RegistrationAndLoginWebsite/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RegistrationAndLoginWebsite.Data;
 
 namespace RegistrationAndLoginWebsite
 {
@@ -31,6 +33,10 @@ namespace RegistrationAndLoginWebsite
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlServer(Configuration.GetConnectionString("UserManagementDb"));
+                });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
