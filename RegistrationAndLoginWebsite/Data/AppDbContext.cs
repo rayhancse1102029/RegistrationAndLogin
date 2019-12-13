3c2290612d4bdf530d9d1a94f1a53ad07297815a
aburayhan <rayhanabu447@gmail.com>
2019-12-13 17:29:16 +0600

    Identity User Manager

--- RegistrationAndLoginWebsite/Data/AppDbContext.cs
+++ RegistrationAndLoginWebsite/Data/AppDbContext.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace RegistrationAndLoginWebsite.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
 
+        //private DbSet<Base> bases { get; set; }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    //modelBuilder.Seed();
+        //}
 
     }
 }
--- RegistrationAndLoginWebsite/Startup.cs
+++ RegistrationAndLoginWebsite/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -36,7 +37,10 @@ namespace RegistrationAndLoginWebsite
             services.AddDbContext<AppDbContext>(options =>
                 {
                     options.UseSqlServer(Configuration.GetConnectionString("UserManagementDb"));
-                });
+            });
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
@@ -57,6 +61,7 @@ namespace RegistrationAndLoginWebsite
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
